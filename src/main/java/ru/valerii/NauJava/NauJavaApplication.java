1b14c920dece5f69a9d8335976d0e955a2ecdd95
Valerii_Butko <lb2005lba@gmail.com>
2026-03-06 15:21:47 +0500

    hw3: реализованы базовые настройки проекта

---
+++ src/main/java/ru/valerii/NauJava/NauJavaApplication.java
@@ -0,0 +1,13 @@
+package ru.valerii.NauJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NauJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NauJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/valerii/NauJava/NauJavaHwApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.valerii.NauJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NauJavaHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

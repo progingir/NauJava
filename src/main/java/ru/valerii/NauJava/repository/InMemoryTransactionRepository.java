1c21b0c814082cbe371f8956f6cd44120a3bc180
Valerii_Butko <lb2005lba@gmail.com>
2026-03-06 15:44:20 +0500

    hw3: реализован слой доступа к данным

---
+++ src/main/java/ru/valerii/NauJava/repository/InMemoryTransactionRepository.java
@@ -0,0 +1,49 @@
+package ru.valerii.NauJava.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.valerii.NauJava.entity.Transaction;
+
+import java.util.List;
+
+@Repository
+public class InMemoryTransactionRepository implements CrudRepository<Transaction> {
+
+    private final List<Transaction> database;
+
+    public InMemoryTransactionRepository(List<Transaction> database) {
+        this.database = database;
+    }
+
+    @Override
+    public void create(Transaction entity) {
+        database.add(entity);
+    }
+
+    @Override
+    public Transaction read(Long id) {
+        return database.stream()
+                .filter(t -> t.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public List<Transaction> readAll() {
+        return database;
+    }
+
+    @Override
+    public void update(Transaction entity) {
+        Transaction existing = read(entity.getId());
+        if (existing != null) {
+            existing.setAmount(entity.getAmount());
+            existing.setCurrency(entity.getCurrency());
+            existing.setDescription(entity.getDescription());
+        }
+    }
+
+    @Override
+    public void delete(Long id) {
+        database.removeIf(t -> t.getId().equals(id));
+    }
+}
